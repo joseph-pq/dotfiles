17bef21a11b83afd9a5d0cba500a84e53520aebc
Joseph P. <joseph.pena.q@gmail.com>
2024-10-31 09:44:51 -0300

    🔧(nvim): set kanagawa as theme

--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -6,8 +6,15 @@ return {
         priority = 1000, -- make sure to load this before all the other start plugins
         config = function()
             -- load the colorscheme here
-            vim.cmd([[colorscheme tokyonight]])
+            -- vim.cmd([[colorscheme tokyonight]])
         end,
     },
-
+    {
+        "rebelot/kanagawa.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd([[colorscheme kanagawa]])
+        end,
+    },
 }
