7d036b488c864a125ad116b3a08837b6d71e5820
Joseph P. <joseph.pena.q@gmail.com>
2025-01-03 16:54:30 -0300

    🐛 use a node script

--- .config/zfunc/lazy-nvm.sh
+++ .config/zfunc/lazy-nvm.sh
@@ -1,18 +1,17 @@
 function lazy_nvm {
   unset -f nvm
   unset -f npm
-  unset -f node
+  # unset -f node
   unset -f npx
 
   if [ -d "${HOME}/.nvm" ]; then
-    export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # linux
-    [ -s "${NVM_DIR}/nvm.sh" ] && source ${NVM_DIR}/nvm.sh # osx
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
   fi
 }
 
 # aliases
 function nvm { lazy_nvm; nvm "$@"; }
 function npm { lazy_nvm; npm "$@"; }
-function node { lazy_nvm; node "$@"; }
+# function node { lazy_nvm; node "$@"; }
 function npx { lazy_nvm; npx "$@"; }
